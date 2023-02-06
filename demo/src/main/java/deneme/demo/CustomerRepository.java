941dc620d1e0b708c22bdcb4cedc107f8b2157a0
muhmerk <muhmerk@gmail.com>
2023-02-06 23:42:10 +0300

    Add files via upload

---
+++ demo/src/main/java/deneme/demo/CustomerRepository.java
@@ -0,0 +1,8 @@
+package deneme.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+
+    Customer findCustomerById(Integer id);
+}
